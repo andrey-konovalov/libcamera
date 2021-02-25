andrey-konovalov/libcamera

commit 89fb1efac240451a2e463a405f4676910fc062b1

libcamera: simple: Fill stride and frameSize at config validation
Fill the stride and frameSize fields of the StreamConfiguration at
configuration validation time instead of at camera configuration time.
This allows applications to get the stride when trying a configuration
without modifying the active configuration of the camera.

Signed-off-by: Paul Elder <paul.elder@ideasonboard.com>
Reviewed-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
Reviewed-by: Jacopo Mondi <jacopo@jmondi.org>

src/libcamera/pipeline/simple/converter.cpp
@@ -261,4 +261,19 @@ void SimpleConverter::outputBufferReady(FrameBuffer *buffer)
 	}
 }
 
+std::tuple<unsigned int, unsigned int>
+SimpleConverter::strideAndFrameSize(const Size &size,
+				    const PixelFormat &pixelFormat)
+{
+	V4L2DeviceFormat format = {};
+	format.fourcc = m2m_->capture()->toV4L2PixelFormat(pixelFormat);
+	format.size = size;
+
+	int ret = m2m_->capture()->tryFormat(&format);
+	if (ret < 0)
+		return { 0, 0 };
+
+	return { format.planes[0].bpl, format.planes[0].size };
+}
+
 } /* namespace libcamera */

src/libcamera/pipeline/simple/converter.h
@@ -10,6 +10,7 @@
 
 #include <memory>
 #include <queue>
+#include <tuple>
 #include <vector>
 
 #include <libcamera/pixel_format.h>
@@ -45,6 +46,9 @@ public:
 	void stop();
 
 	int queueBuffers(FrameBuffer *input, FrameBuffer *output);
+
+	std::tuple<unsigned int, unsigned int>
+	strideAndFrameSize(const Size &size, const PixelFormat &pixelFormat);
 
 	Signal<FrameBuffer *, FrameBuffer *> bufferReady;
 

src/libcamera/pipeline/simple/simple.cpp
@@ -457,6 +457,30 @@ CameraConfiguration::Status SimpleCameraConfiguration::validate()
 
 	cfg.bufferCount = 3;
 
+	/* Set the stride and frameSize. */
+	if (!needConversion_) {
+		V4L2DeviceFormat format = {};
+		format.fourcc = data_->video_->toV4L2PixelFormat(cfg.pixelFormat);
+		format.size = cfg.size;
+
+		int ret = data_->video_->tryFormat(&format);
+		if (ret < 0)
+			return Invalid;
+
+		cfg.stride = format.planes[0].bpl;
+		cfg.frameSize = format.planes[0].size;
+
+		return status;
+	}
+
+	SimplePipelineHandler *pipe = static_cast<SimplePipelineHandler *>(data_->pipe_);
+	SimpleConverter *converter = pipe->converter();
+
+	std::tie(cfg.stride, cfg.frameSize) =
+		converter->strideAndFrameSize(cfg.size, cfg.pixelFormat);
+	if (cfg.stride == 0)
+		return Invalid;
+
 	return status;
 }
 
@@ -556,8 +580,6 @@ int SimplePipelineHandler::configure(Camera *camera, CameraConfiguration *c)
 			<< videoFormat.toString();
 		return -EINVAL;
 	}
-
-	cfg.stride = captureFormat.planes[0].bpl;
 
 	/* Configure the converter if required. */
 	useConverter_ = config->needConversion();

commit 0a1d25c3777c5e58a9602f2b62e1f5ac1d698216

libcamera: pipeline: simple: Don't override stride at configure time
The stride (and frame size) calculation has been moved from configure
time to configuration validate time by commit 89fb1efac240 ("libcamera: simple:
Fill stride and frameSize at config validation"). This change has
however left one stray setting of the stride when configuring the
converter. Fix it.

While at it, turn the SimpleConverter::configure() output configuration
argument to a const reference to emphasize it can't be null and isn't
modified by the function, and rename it from cfg to outputCfg to make
its purpose clearer.

Fixes: 89fb1efac240 ("libcamera: simple: Fill stride and frameSize at config validation")
Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>

src/libcamera/pipeline/simple/converter.cpp
@@ -134,7 +134,7 @@ SizeRange SimpleConverter::sizes(const Size &input)
 }
 
 int SimpleConverter::configure(PixelFormat inputFormat, const Size &inputSize,
-			       StreamConfiguration *cfg)
+			       const StreamConfiguration &outputCfg)
 {
 	V4L2DeviceFormat format;
 	int ret;
@@ -157,10 +157,10 @@ int SimpleConverter::configure(PixelFormat inputFormat, const Size &inputSize,
 	}
 
 	/* Set the pixel format and size on the output. */
-	videoFormat = m2m_->capture()->toV4L2PixelFormat(cfg->pixelFormat);
+	videoFormat = m2m_->capture()->toV4L2PixelFormat(outputCfg.pixelFormat);
 	format = {};
 	format.fourcc = videoFormat;
-	format.size = cfg->size;
+	format.size = outputCfg.size;
 
 	ret = m2m_->capture()->setFormat(&format);
 	if (ret < 0) {
@@ -169,13 +169,11 @@ int SimpleConverter::configure(PixelFormat inputFormat, const Size &inputSize,
 		return ret;
 	}
 
-	if (format.fourcc != videoFormat || format.size != cfg->size) {
+	if (format.fourcc != videoFormat || format.size != outputCfg.size) {
 		LOG(SimplePipeline, Error)
 			<< "Output format not supported";
 		return -EINVAL;
 	}
-
-	cfg->stride = format.planes[0].bpl;
 
 	return 0;
 }

src/libcamera/pipeline/simple/converter.h
@@ -37,7 +37,7 @@ public:
 	SizeRange sizes(const Size &input);
 
 	int configure(PixelFormat inputFormat, const Size &inputSize,
-		      StreamConfiguration *cfg);
+		      const StreamConfiguration &outputCfg);
 	int exportBuffers(unsigned int count,
 			  std::vector<std::unique_ptr<FrameBuffer>> *buffers);
 

src/libcamera/pipeline/simple/simple.cpp
@@ -604,7 +604,7 @@ int SimplePipelineHandler::configure(Camera *camera, CameraConfiguration *c)
 
 	if (useConverter_) {
 		ret = converter_->configure(pipeConfig.pixelFormat,
-					    pipeConfig.captureSize, &cfg);
+					    pipeConfig.captureSize, cfg);
 		if (ret < 0) {
 			LOG(SimplePipeline, Error)
 				<< "Unable to configure converter";